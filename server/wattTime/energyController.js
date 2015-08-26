423371b035b6253e6fcbaff7e82d747dea52c28d
Kate Cobb <KateEcobb@gmail.com>
2015-08-26 10:38:51 -0700

    update function finished, working on helper functions

--- server/wattTime/energyController.js
+++ server/wattTime/energyController.js
@@ -1,48 +1,57 @@
 var TotalEnergy = require('./energyModel')
 var request = require('request');
 
-
-var update = function(req, res, next, dateStart, dateEnd){ 
-
+/* update queries the WattTime API and continually updates the database with new information 
+* from the last 24 hours.
+*/
+var update = function(dateStart, dateEnd){ 
+  var holder;
   //query WattTime API for info from last 24 hours
   request('http://api.watttime.org:80/api/v1/datapoints/?ba=\
-    CAISO&start_at='+dateStart+'&end_at='+dateEnd+'&page_size=1000&market=RT5M', function(err, response, body){ 
-      if(!err && res.statusCode === 200){ 
-        var data = JSON.parse(body);
-        //check to see if id is in energy database, have to iterate through results array
-        for(var i=0; i<data.results.length; i++){ 
-          TotalEnergy.find({ 
-            timestamp: data.results[i].timestamp
-          }).exec(function(err, result){ 
-            if(err) console.log("Error in retrieving data " + err);
-            if(result === []){ 
-      
-              var newEnergy = new TotalEnergy({ 
-                timestamp: data.results[i].timestamp,
-                created_at: data.results[i].created_at, 
-                carbon: data.results[i].carbon, 
-                genmix: [{ 
-                  fuel: data.results[i].genmix[, 
-                  gen_MW: Number
-                }], 
-                url: String, 
-                market: String, 
-                freq: String, 
-                ba: String 
+  CAISO&start_at='+dateStart+'&end_at='+dateEnd+'&page_size=1000&market=RT5M', function(err, res, body){ 
+      //if no error and API query was OK
+    if(!err && res.statusCode === 200){ 
+      var data = JSON.parse(body);
 
+      //iterate through results array, check to see if timestamp is in energy database
+      for(var i=0; i<data.results.length; i++){ 
+        TotalEnergy.find({ 
+          timestamp: data.results[i].timestamp
+        }).exec(function(err, result){ 
+          if(err) console.log("Error in retrieving data from TotalEnergy " + err);
+          
+          //if an empty array, i.e. nothing returned from the database
+          if(result.length === 0){ 
+            
+            //iterate through genmix data, push into a holder
+            holder = []
+            for(var j=0; j<data.results[i].genmix.length; j++){ 
+              holder.push({ 
+                fuel: data.results[i].genmix.fuel, 
+                gen_MW: data.results[i].genmix.gen_MW
               })
-
             }
 
-
-          })
+            //add model to document
+            var newEnergy = new TotalEnergy({ 
+              timestamp: data.results[i].timestamp,
+              created_at: data.results[i].created_at, 
+              carbon: data.results[i].carbon, 
+              genmix: holder, 
+              url: data.results[i].url, 
+              market: data.results[i].market, 
+              freq: data.results[i].freq, 
+              ba: data.results[i].ba
+            })
+            
+            //reset holder
+            holder = [];
+          }
+        })
       }
     }
-
   })
-
-
-
-
-
 };
+
+
+
