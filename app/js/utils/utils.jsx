f6cd74527f5eaa761d1c6839024b6886a84f3f2a
halcyonnx <halcyonnx@github.com>
2015-08-25 18:55:59 -0700

    [Feature] Closed #10 added in util.js to do server requests

---
+++ app/js/utils/utils.jsx
@@ -0,0 +1,21 @@
+var routes = require('../constants/Constants.js').ServerRoutes;
+
+modules.exports = {
+  getDataPoints : function() {
+    return new Promise(function(resolve, reject) {
+
+      $.ajax({
+        url: routes.DATA_SOURCE,
+        method: 'GET',
+        dataType: 'json',
+        success: function(data) {
+          resolve(data);
+        },
+        error: function(xhr, status, err) {
+          reject(err);
+        }
+      });
+
+    });
+  };
+};
