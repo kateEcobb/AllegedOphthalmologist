4c7bc7a260cfc54a0cedada7f9267dbaf859a47b
halcyonnx <halcyonnx@github.com>
2015-08-28 17:22:34 -0700

    [Feature] Have converted x-axis to time scale, need to clean up code and refactor out

--- app/js/components/LineGraphView.jsx
+++ app/js/components/LineGraphView.jsx
@@ -36,7 +36,7 @@ var LineGraphView = React.createClass({
 
   componentDidUpdate: function() {
     var el = React.findDOMNode(this);
-    console.log(this.state.data.Watt);
+    console.log(this.state.data.Watt[0]);
     this.createGraph(el, {
       width: '1000',
       height: '500',
@@ -60,16 +60,42 @@ var LineGraphView = React.createClass({
   // Class Functions ////////////////////////////////////
 
   createGraph: function(el, props, state) {
-    var data = state.data.Watt;
+
+    // Parse out the required data from the state
+    var data = [];
+    var dataTime = [];
+    for (var i = 0; i < state.data.Watt.length; i++) {
+      data.push({
+        carbon: parseInt(state.data.Watt[i].carbon),
+        time: new Date(state.data.Watt[i].timestamp),
+        id: state.data.Watt[i]._id
+      });
+    }
+
+    for (var i = 0; i < state.data.Watt.length; i++) {
+      dataTime.push({
+        carbon: parseInt(state.data.Watt[i].carbon),
+        time: state.data.Watt[i].timestamp
+      });
+    }
+
+    // Sort the data by time
+    data.sort(function(a, b) {
+      return a.time - b.time;
+    });
+
+    dataTime.sort(function(a, b) {
+      return new Date(a.time) - new Date(b.time);
+    })
+
+    // Set up the graph dimensions
     var scale = {
       height: parseInt(props.height, 10),
       width: parseInt(props.width, 10),
       margin: parseInt(props.margin, 10)
     };
-    var height = parseInt(props.height, 10);
-    var width = parseInt(props.width, 10);
-    var margin = parseInt(props.margin, 10);
 
+    // Set up the graph
     var graph = d3.select(el).append('svg:svg')
               .attr('class', 'svg')
               .attr('width', scale.width + scale.margin + scale.margin)
@@ -77,42 +103,61 @@ var LineGraphView = React.createClass({
               .append('svg:g')
                 .attr('transform', 'translate(' + (scale.margin) + ',' + (scale.margin) + ')');
 
-    this.createScale(graph, scale, data);
+    this.createScale(graph, scale, data, dataTime);
   },
 
-  createScale: function(graph, scale, data) {
+  createScale: function(graph, scale, data, dataTime) {
 
-    // Addtional parameters we need to calculate to create the x and y axis and pass to draw the line
-    scale.axisOffset = 50;
-    scale.yRange = d3.scale.linear().domain([0, 2000]).range([scale.height-100, 0]);
-    scale.xRange = d3.scale.linear().domain([0, data.length]).range([0, scale.width-100]);
+    // Additional parameters we need to calculate to create the x and y axis and pass to draw the line
+    scale.axisOffset = 50;   // Inner offset used as margins on either side in order to give the axis space to show
+    scale.yMaxRatio = 1.05;  // Ratio we use to give some extra padding to the top of the graph
+
+    // Ranges we need to scale the datapoints
+    scale.yRange = d3.scale.linear().domain([d3.min(data, function(datum) {
+      return datum.carbon;
+    }), d3.max(data, function(datum) {
+      return datum.carbon * scale.yMaxRatio;  
+    })])
+    .range([scale.height - scale.axisOffset - scale.axisOffset, 0]);
+
+    scale.xRange = d3.scale.linear().domain([d3.min(data, function(datum) {
+      return datum.time;
+    }), d3.max(data, function(datum) {
+      return datum.time;
+    })])
+    .range([0, scale.width - scale.axisOffset - scale.axisOffset]);
+
+    scale.xTimeRange = d3.time.scale.utc().domain([data[0].time, data[data.length - 1].time])
+    .range([0, scale.width - scale.axisOffset - scale.axisOffset]);
 
     var xAxis = d3.svg.axis().scale(scale.xRange);
+
+    var xTimeAxis = d3.svg.axis().scale(scale.xTimeRange);
 
     var yAxis = d3.svg.axis().scale(scale.yRange).orient('left');
 
     graph.append('svg:g')
-    .attr('class', 'x Axis')
+    .attr('class', 'x axis')
     .attr('transform', 'translate(' + (scale.axisOffset) + ',' + (scale.height - scale.axisOffset) + ')')
-    .call(xAxis);
+    .call(xTimeAxis);
 
     graph.append('svg:g')
-    .attr('class', 'y Axis')
+    .attr('class', 'y axis')
     .attr('transform', 'translate(' + (scale.axisOffset) + ',' + (scale.axisOffset) + ')')
     .call(yAxis);
 
-
-    this.drawGraph(graph, scale, data);
+    this.drawPoints(graph, scale, data);
+    this.drawGraph(graph, scale, data, dataTime);
   },
 
-  drawGraph: function(graph, scale, data) {
+  drawGraph: function(graph, scale, data, dataTime) {
 
     var lineFunc = d3.svg.line()
                     .x(function(datum, i) {
-                      return scale.xRange( i );
+                      return scale.xTimeRange( datum.time );
                     })
                     .y(function(datum) {
-                      return scale.yRange( parseInt(datum.carbon) );
+                      return scale.yRange( datum.carbon );
                     })
                     .interpolate('linear');
 
@@ -124,64 +169,29 @@ var LineGraphView = React.createClass({
     .attr('transform', 'translate(' + (scale.axisOffset) + ', 0)' );
   },
 
-  testGraph: function(el) {
-    var m = [80, 80, 80, 80]; // margins
-    var w = 1000 - m[1] - m[3]; // width
-    var h = 400 - m[0] - m[2]; // height
-    
-    // create a simple data array that we'll plot with a line (this array represents only the Y values, X will just be the index location)
-    var data = [3, 6, 2, 7, 5, 2, 0, 3, 8, 9, 2, 5, 9, 3, 6, 3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 9, 2, 7];
+  drawPoints: function(graph, scale, data) {
+    var pointGraph = graph.append('svg:g')
+                      .attr('class', 'pointGraph')
+                      .attr('transform', 'translate(' + (scale.axisOffset) + ',' + (0) + ')');
+    console.log('Data', data);
+    // DATA JOIN
+    var points = pointGraph.selectAll('circle')
+                  .data(data, function(datum) {return datum.id;});
 
-    // X scale will fit all values from data[] within pixels 0-w
-    var x = d3.scale.linear().domain([0, data.length]).range([0, w]);
-    // Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
-    var y = d3.scale.linear().domain([0, 10]).range([h, 0]);
-      // automatically determining max range can work something like this
-      // var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
+    // DATA UPDATE
 
-    // create a line function that can convert data[] into x and y points
-    var line = d3.svg.line()
-      // assign the X function to plot our line as we wish
-      .x(function(d,i) { 
-        // verbose logging to show what's actually being done
-        console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
-        // return the X coordinate where we want to plot this datapoint
-        return x(i); 
-      })
-      .y(function(d) { 
-        // verbose logging to show what's actually being done
-        console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
-        // return the Y coordinate where we want to plot this datapoint
-        return y(d); 
-      })
+    // ENTER
+    points.enter().append('circle')
+    .attr('class', 'linePoint')
+    .attr('cx', function(datum) {
+      return scale.xTimeRange( datum.time ); 
+    })
+    .attr('cy', function(datum) {
+      return scale.yRange( datum.carbon ); 
+    });
 
-    // Add an SVG element with the desired dimensions and margin.
-    var graph = d3.select(el).append("svg:svg")
-          .attr("width", w + m[1] + m[3])
-          .attr("height", h + m[0] + m[2])
-        .append("svg:g")
-          .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
-
-    // create yAxis
-    var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
-    // Add the x-axis.
-    graph.append("svg:g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + h + ")")
-          .call(xAxis);
-
-
-    // create left yAxis
-    var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
-    // Add the y-axis to the left
-    graph.append("svg:g")
-          .attr("class", "y axis")
-          .attr("transform", "translate(-25,0)")
-          .call(yAxisLeft);
-    
-    // Add the line by appending an svg:path element with the data line we created above
-    // do this AFTER the axes above so that the line is above the tick-lines
-    graph.append("svg:path").attr("d", line(data));
+    // EXIT
+    points.exit().remove();
   }
 
 });
