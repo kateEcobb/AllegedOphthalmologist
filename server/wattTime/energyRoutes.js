4e9164b7cb3513fd2c5085e75cb7732ede198e03
Kate Cobb <KateEcobb@gmail.com>
2015-08-31 19:13:35 -0700

    [feat] DAHR cache finished.

--- server/wattTime/energyRoutes.js
+++ server/wattTime/energyRoutes.js
@@ -2,4 +2,6 @@ var energyController = require('./energyController');
 
 module.exports = function(app){ 
   app.get('/api/get24HourBehind', energyController.get24HourBehind);
+  app.get('/api/get24HourAhead', energyController.get24HourAhead);
+
 };
