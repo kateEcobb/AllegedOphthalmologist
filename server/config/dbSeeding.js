3a0b61b982b582836e6fb076f92caf15c3bbd3ae
Kate Cobb <KateEcobb@gmail.com>
2015-09-01 15:49:20 -0700

    [code] database seeding working correctly.

---
+++ server/config/dbSeeding.js
@@ -0,0 +1,64 @@
+module.exports = {
+
+  Drew: { 
+    username: "admin1",
+    password: "tightHips",
+    token: null,
+    utilityAPIData: { 
+      account_auth: "Andrew Weber",
+      account_uid: "7878",
+      service_uid: "13984", 
+      PGE_username: "DieselTrain",
+      bill_count: 24, 
+      utility: "PG&E", 
+      utility_service_address: "470 RICH ST, OAKLAND, CA 94609-2513704"
+    }
+  },
+
+  Brandon: { 
+    username: "admin2",
+    password: "tightHips",
+    token: null,
+    utilityAPIData: { 
+      account_auth: "Brandon McKane",
+      account_uid: "7881",
+      service_uid: "13989", 
+      PGE_username: "FullThrottle",
+      bill_count: 0, 
+      utility: "PG&E", 
+      utility_service_address: "828 NORVELL ST, EL CERRITO, CA 94530-2945289"
+    }
+  },
+
+  JD: { 
+    username: "admin3",
+    password: "tightHips",
+    token: null,
+    utilityAPIData: { 
+      account_auth: "John Doe",
+      account_uid: "7965",
+      service_uid: "14591", 
+      PGE_username: "testy",
+      bill_count: 13, 
+      utility: "DEMO", 
+      utility_service_address: "123 Main St #100, Anytown, CA 94612"
+    }
+  },
+
+  John: { 
+    username: "admin4",
+    password: "tightHips",
+    token: null,
+    utilityAPIData: { 
+      account_auth: "John Olmsted",
+      account_uid: "8065",
+      service_uid: "14825", 
+      PGE_username: "Horsies",
+      bill_count: 0, 
+      utility: "PG&E", 
+      utility_service_address: "2436 CALIFORNIA ST # B, BERKELEY, CA 94703-1612368"
+    }
+  }
+};
+
+
