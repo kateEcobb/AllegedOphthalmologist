d4ce63030844a554802bff70a8635c550759b120
Kate Cobb <KateEcobb@gmail.com>
2015-09-03 20:03:25 -0700

    Working on getting blur to render correctly.

--- app/js/components/bulbGlow.js
+++ app/js/components/bulbGlow.js
@@ -5,13 +5,23 @@ var makeCircle = function(el, props, state){
   var svgContainer = d3.select(el).append('svg:svg')
                     .attr('className', 'bulbGlowContainer')
                     .attr('width', props.width)
-                    .attr('height', props.height);
+                    .attr('height', props.height)
+                    .style('left', '19px');
+
+  var defs = svgContainer.append('defs')
+  var filter = defs.append('filter')
+                  .attr('id', 'gaussian')
+                  .append('feGaussianBlur')
+                  .attr('in', 'SourceAlpha')
+                  .attr('stdDeviation',3)
+                  .attr('result','blur')
 
   var circle = svgContainer.append('circle')
                           .attr('className', 'bulbGlow')
                           .attr('cx', 30)
                           .attr('cy', 30)
                           .attr('r', 20)
+                          // .attr('filter','url(#gaussian)')
                           .style('fill', state.toString())
   return;
 };
