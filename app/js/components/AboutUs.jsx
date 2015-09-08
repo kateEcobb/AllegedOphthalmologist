af69681e5d264dac2dbe93a692a4237f768cffd6
Kate Cobb <KateEcobb@gmail.com>
2015-09-07 18:38:49 -0700

    more styling.

--- app/js/components/AboutUs.jsx
+++ app/js/components/AboutUs.jsx
@@ -66,33 +66,51 @@ var AboutUs = React.createClass({
   },
 
   render: function(){ 
-    console.log(this.state.data);
     var donuts = 'donuts';
     return ( 
       <div className="frontPage"> 
 
-      <header className="image-bg-fluid-height">
+      <header className="image-bg-fluid-height" id='home'>
         <BulbView loadModal={false} name={"bulbContainer2"} SVGname={"bulb2"} height={400} width={400} margin={5} cx={75} cy={75} r={75} />
       </header>
 
-      <section>
+
+      <section id='about' className='pad-section'>
+        <div className='container'>
+          <div className="row">
+            <div className="col-sm-6">
+              <h1 className="section-heading">GridAware</h1>
+            </div>
+            <div className="col-sm-6 text-center">
+              <p className="lead section-lead">Compare your Energy Use to Current Grid Conditions</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id='cont1'>
         <div className="container">
             <div className="row">
                 <div className="col-lg-12">
-                    <h1 className="section-heading">GridAware</h1>
-                    <p className="lead section-lead">Compare your Energy Use to Current Grid Conditions</p>
-                    <Tabs>
-                      <Tab label="About Us"> 
+                      <h2>About Us</h2>
                       <p className='lead section-lead'> All power is not created equal. </p>
                       <div className={donuts} style={{'maxWidth':'400px'}}></div>
-                      <p className='section-paragraph'> These pie graphs represent the current real-time breakdown of resources used to 
-                      fuel California's power grid. "Other" is a mix of coal, nuclear, natural gas, and other carbon-producing fuels.</p>
+              
                       <p className='section-paragraph'> GridAware empowers consumers to use energy more intelligently and reduce their carbon footprint by providing 
                       frictionless access to current grid conditions. We are thrilled to introduce the GridAware Power Bulb -- 
                       a WiFi-enabled LED bulb that changes color based on energy cleanliness. Put it anywhere in your house or apartment 
                       and start checking the state of California’s grid at a glance!</p>
-                      </Tab>
-                      <Tab label="How Does The Grid Work!?">
+
+                </div>
+            </div>
+        </div>
+      </section>
+    <div id='information' className='pad-section'>
+      <section id='cont2'> 
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                  <h2>How Does The Grid Work!?</h2>
                         <p className="section-paragraph">Each day, the mix of energy generation resources changes as demand fluctuates. 
                         The grid operator must efficiently balance power generation and consumer electricity demand, 
                         using renewable resources whenever possible as conditions allow.</p>
@@ -100,9 +118,17 @@ var AboutUs = React.createClass({
                         <p className="section-paragraph">Periods of high demand and low availability of renewable generation resources translate to more pollution 
                         associated with each unit of energy consumed. Periods of low demand and high availability of 
                         renewable resources mean less pollution is associated with the same unit of energy.</p>
-                      </Tab>
-                      <Tab label="Get Started!"> 
-                      <p className="section-paragraph">To get started, use our REGISTRATION page to signup for GridAware, 
+              </div>
+            </div>
+        </div>
+      </section>
+
+      <section id='cont3'> 
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <h2>Get Started!</h2> 
+                      <p className="section-paragraph">To get started, use our Registration page to signup for GridAware, 
                       and link  your SmartMeter at the same time! All you need is your PG&E login.</p>
 
                       <p className="section-paragraph">Don't have an online account with PG&E yet? 
@@ -111,16 +137,11 @@ var AboutUs = React.createClass({
                       <p className="section-paragraph">Once you register, our robots will analyze your energy consumption.
                       This data usually takes approximately 24 hours to become available. Once linked, your hourly consumption 
                       data is typically updated daily.</p>
-                      </Tab>
-
-                    </Tabs>
-
-                </div>
+              </div>
             </div>
         </div>
       </section>
-
-        
+        </div>
       </div>
     )
 
