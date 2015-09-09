9b95e90987960971a3715094de8bf9e5229fcd07
johndrkurtcom <john@drkurt.com>
2015-09-08 17:13:15 -0700

    (tests) server route test are almost done

--- server/tests/__tests__.spec.js
+++ server/tests/__tests__.spec.js
@@ -5,76 +5,129 @@ var userController = require('../users/userController');
 describe('Routes', function(){
   var token;
 
-  beforeEach(function(){
-    var getToken = function(){
-      request.post(
-        {
-          header: {'content-type': 'application/x-www-form-urlencoded'},
-          url: 'http://127.0.0.1:8080/signin', 
-          json: {username: "admin1@blah.com", password: "tightHips"}
-        }, function(error, response, body){
-          if(error) console.log(error.message);
-          token = body.token;
-      });
-    };
-
-    runs(getToken);
-
-    waitsFor(function(){
-      return token;
+  var signin = function(callback){
+    request.post(
+      {
+        header: {'content-type': 'application/x-www-form-urlencoded'},
+        url: 'http://127.0.0.1:8080/signin', 
+        json: {username: "admin1@blah.com", password: "tightHips"}
+      }, function(error, response, body){
+        if(error) console.log(error.message);
+        // console.log(token);
+        token = body.token;
+        callback(token);
     });
-  });
+  }
 
-  it('should have route for index', function(){
+  signin(function(){return true});
+
+  var signout = function(callback){
+    request.get({
+      header: {'content-type': 'application/x-www-form-urlencoded'},
+      url:'http://127.0.0.1:8080/logout',
+      headers: {'Authorization': 'Bearer ' + token},
+    }, function(error, response, body){
+      callback(error, response, body);
+    });
+  }
+
+  it('should have route for index', function(done){
     request('http://127.0.0.1:8080/', function(error, response, body){
-      // console.log(typeof body, typeof response);
       expect(typeof body).toEqual('string');
+      done();
     });
   });
 
-  xit('should have POST route for signup', function(){
-    request('http://127.0.0.1:8080/', function(error, response, body){
-      expect(response).toBe(json);
+  //almost working but still needs work
+  xit('should have signup route', function(done){
+    request.post({
+      header: {'content-type': 'application/x-www-form-urlencoded'},
+      url: 'http://127.0.0.1:8080/signup',
+      json: {
+        username: "admin3@blah.com",
+        password: "tightHips",
+        utility_username: "testy",
+        utility_password: 'tightHips',
+        pgeFullName: 'John Doe'
+      }
+    }, function(error, response, body){
+      expect(response.statusCode).toEqual(200);
+      done();
     });
   });
 
-  it('should have POST route for signin', function(){
-    expect(token).toBeDefined();
+  it('should not let multiple people with same name signup', function(done){
+    request.post({
+      header: {'content-type': 'application/x-www-form-urlencoded'},
+      url: 'http://127.0.0.1:8080/signup',
+      json: {
+        username: "admin4@blah.com",
+        password: "tightHips",
+        utility_username: "Horsies",
+        utility_password: ''
+      }
+    }, function(error, response, body){
+      expect(response.statusCode).toEqual(418);
+      done();
+    });
+  });
+
+  it('should have POST route for signin', function(done){
+      console.log(token, 'signin');
+      expect(token).toBeDefined();
+      done();
+  }); 
+
+  it('should handle incorrect credintials', function(done){
+    console.log('bad creds');
+    signout(function(){
+      request.post(
+      {
+        header: {'content-type': 'application/x-www-form-urlencoded'},
+        url: 'http://127.0.0.1:8080/signin', 
+        json: {username: "blah@blah.com", password: "tightlskdjfl"}
+      }, function(error, response, body){
+        if(error) console.log('=================', error.message);
+        expect(response.statusCode).toEqual(418);
+        signin(function(){done()});
+      });
+    })
   });
 
   it('should have GET route for users meterreadings', function(done){
+    console.log(token, 'meterreadings');
     request.get({
       header: {'content-type': 'application/x-www-form-urlencoded'},
       headers: {'Authorization': 'Bearer ' + token},
       url: 'http://127.0.0.1:8080/api/user/meterreadings/',
       json: {service_uid: 13984},
     }, function(error, response, body){
-      // console.log(body,'==========================');
       expect(Array.isArray(body)).toEqual(true);
       done();
     })
   });
 
-  it('should have POST route for changing PG&E info', function(done){
+  //sort of working will come back to it later
+  xit('should have POST route for changing PG&E info', function(done){
     userController.changePGEData = jasmine.createSpy('test');
-
-
     request.post({
       header: {'content-type': 'application/x-www-form-urlencoded'},
       headers: {'Authorization': 'Bearer ' + token},
       url: 'http://127.0.0.1:8080/api/user/changePGE',
-      json: {stuff: 'stuff'}
+      json: {}
     }, function(error, response, body){
     })
     expect(userController.changePGEData).toHaveBeenCalled();
     done();
-
   });
 
-  xit('should have GET route for logout', function(){
-    request('http://127.0.0.1:8080/', function(error, response, body){
-      expect(response).toBe(json);
-    });
+
+  it('should have GET route for logout', function(done){
+    // console.log(token, 'logout');
+    signout(function(error, response, body){
+      expect(body).toEqual('"Redirect to home"');
+      done();
+    })
   });
 
 });
